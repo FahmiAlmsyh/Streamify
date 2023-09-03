902f757ede81eb7ecd559a9ffd70fe8e32acb4df
FahmiAlmsyh <alamsyahf100@gmail.com>
2023-09-03 14:21:35 +0700

    add

--- main.js
+++ main.js
@@ -36,3 +36,40 @@ const handleFirstTab = (e) => {
       alterStyles(isBackToTopRendered);
     }
   });
+
+
+// button show
+const toggleButton = document.getElementById('toggleButton');
+const content = document.getElementById('content');
+
+let isContentVisible = false;
+
+toggleButton.addEventListener('click', () => {
+if (!isContentVisible) {
+content.style.display = 'block';
+isContentVisible = true;
+toggleButton.innerHTML = 'Less Series <i class="bx bx-chevron-up"></i>';
+} else {
+content.style.display = 'none';
+isContentVisible = false;
+toggleButton.innerHTML = 'About the Series <i class="bx bx-chevron-down"></i>';
+}
+});
+// dropdown
+const dropdownButton = document.getElementById('dropdownButton');
+const dropdownContent = document.getElementById('dropdownContent');
+
+dropdownButton.addEventListener('click', () => {
+if (dropdownContent.style.display === 'block') {
+dropdownContent.style.display = 'none';
+} else {
+dropdownContent.style.display = 'block';
+}
+});
+
+// Close the dropdown when clicking outside of it
+document.addEventListener('click', (event) => {
+if (!dropdownButton.contains(event.target) && !dropdownContent.contains(event.target)) {
+dropdownContent.style.display = 'none';
+}
+});
